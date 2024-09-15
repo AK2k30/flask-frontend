27b3bbdbb0ee2155350f6fad673c07a6ad87ae68
AK2k30 <akashsunilsingh5555@gmail.com>
2024-09-15 11:25:31 +0530

    Refactor test files and remove unused code

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { Todo } from "./Todo";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { EditTodoForm } from "./EditTodoForm";
+import { Trash2 } from "lucide-react";
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo,
+      ),
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo,
+      ),
+    );
+  };
+
+  const deleteAllTodos = () => {
+    setTodos([]);
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>Get Things Done !</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        ),
+      )}
+      {todos.length > 0 && (
+        <button onClick={deleteAllTodos} className="delete-all-btn">
+          <Trash2 size={18} />
+          Delete All
+        </button>
+      )}
+    </div>
+  );
+};
